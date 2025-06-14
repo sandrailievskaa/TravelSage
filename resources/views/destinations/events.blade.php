baf3642e591193c5ac361ac9e8700603fac333d9
Sandra Ilievska <silievska85@yahoo.com>
2025-06-14 16:49:31 +0200

    add cards

--- resources/views/destinations/events.blade.php
+++ resources/views/destinations/events.blade.php
@@ -9,6 +9,15 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro&display=swap" rel="stylesheet">
+    <style>
+        .cardsStyle{
+            display: inline-block;
+            float: right;
+            margin-top: 8%;
+            margin-right: 5%;
+        }
+
+    </style>
 </head>
 <body>
 @include('navbar')
@@ -16,6 +25,32 @@
 <div class="carousel-inner">
     <div class="carousel-item active">
         <img src="{{ asset('images/greece.jpg') }}" class="d-block w-100" alt="...">
+    </div>
+    <div class="cardsStyle">
+        <div class="card border-primary mb-3" style="max-width: 18rem;">
+            <div class="card-header">Header</div>
+            <div class="card-body text-primary">
+                <h5 class="card-title">Primary card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                    card's content.</p>
+            </div>
+        </div>
+        <div class="card border-secondary mb-3" style="max-width: 18rem;">
+            <div class="card-header">Header</div>
+            <div class="card-body text-secondary">
+                <h5 class="card-title">Secondary card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                    card's content.</p>
+            </div>
+        </div>
+        <div class="card border-success mb-3" style="max-width: 18rem;">
+            <div class="card-header">Header</div>
+            <div class="card-body text-success">
+                <h5 class="card-title">Success card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                    card's content.</p>
+            </div>
+        </div>
     </div>
 </div>
 
