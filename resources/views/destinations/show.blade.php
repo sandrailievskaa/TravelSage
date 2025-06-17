ee233be6da3bc717c061907574c49bdce7da1ee3
Sandra Ilievska <silievska85@yahoo.com>
2025-06-17 17:11:17 +0200

    footer edited

--- resources/views/destinations/show.blade.php
+++ resources/views/destinations/show.blade.php
@@ -435,10 +435,6 @@
 @include('footer')
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.8.1/baguetteBox.min.js"></script>
-
-<script>
-    baguetteBox.run('.tz-gallery');
-</script>
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
